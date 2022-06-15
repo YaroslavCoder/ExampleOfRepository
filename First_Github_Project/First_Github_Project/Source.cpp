78882117e6d6cd9d4424c23cfed7d6d5ff073ac5
YaroslavCoder <nepipkapipka8902@gmail.com>
2022-06-15 19:52:44 +0300

    Create project
    created first (empty) project

---
+++ First_Github_Project/First_Github_Project/Source.cpp
@@ -0,0 +1,26 @@
+#include <iostream> // команда для того, чтобы работали все команды С++
+#include <string> // чтобы работали строки
+using namespace std; // команда для доп команд
+
+
+
+
+
+int main()
+{
+	setlocale(LC_ALL, "Russian"); // подключить русский язык
+	cout << "" << endl;
+
+
+
+
+
+
+
+
+
+
+
+	system("pause");
+	return 0;
+}
